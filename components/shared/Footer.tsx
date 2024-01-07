79c7551aa48abce53ca47af7f7f81cadb5059225
subho004 <haiisubhajit2003@gmail.com>
2024-01-07 22:12:13 +0530

    Footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="flex-center flex-between wrapper flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+        <p>2024 Evently. Al Righs Reserved</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
