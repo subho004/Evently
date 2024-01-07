d145ff46d13900880e3dca9bb2bd86707a3dcccf
subho004 <haiisubhajit2003@gmail.com>
2024-01-07 21:13:47 +0530

    Navbar styles responsive

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,46 @@
 import React from "react";
+<<<<<<< Updated upstream
 
 const Header = () => {
   return <div>Header</div>;
+=======
+import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
+
+const Header = () => {
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="Evently logo"
+          />
+        </Link>
+
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
+        <div className="flex w-32 justify-end gap-3"></div>
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+          <MobileNav />
+        </SignedIn>
+        <SignedOut>
+          <Button asChild className="rounded-full" size="lg">
+            <Link href="/sign-in">Login</Link>
+          </Button>
+        </SignedOut>
+      </div>
+    </header>
+  );
+>>>>>>> Stashed changes
 };
 
 export default Header;
