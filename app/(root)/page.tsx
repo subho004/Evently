ad435863457b8b157471ad32a24de3adb30de95a
subho004 <haiisubhajit2003@gmail.com>
2024-01-07 20:01:24 +0530

    Initial Setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl">Evently</h1>
+      <Button variant="destructive">Hello</Button>
+    </main>
+  );
+}
